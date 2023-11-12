fb13d8e45cabc28c843f537a61e33422349f1fe1
pol <pol.armaza.002@gmail.com>
2023-11-11 23:58:27 -0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
-import History from "./pages/history";
+import History from "./pages/History";
 
 function App() {
   return (
